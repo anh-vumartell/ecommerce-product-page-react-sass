83135810b997eabd88021a851a2d214aeb33b3e1
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-12-23 10:45:07 +0200

    add dataImg file

---
+++ src/dataImg.jsx
@@ -0,0 +1,29 @@
+import productOne from "./images/image-product-1.jpg";
+import productTwo from "./images/image-product-2.jpg";
+import productThree from "./images/image-product-3.jpg";
+import productFour from "./images/image-product-4.jpg";
+
+const img = [
+  {
+    url: productOne,
+    urlThumb: "./images/image-product-1-thumbnail.jpg",
+    name: "Product 1",
+  },
+  {
+    url: productTwo,
+    urlThumb: "./images/image-product-2-thumbnail.jpg",
+    name: "Product 2",
+  },
+  {
+    url: productThree,
+    urlThumb: "./images/image-product-3-thumbnail.jpg",
+    name: "Product 3",
+  },
+  {
+    url: productFour,
+    urlThumb: "./images/image-product-4-thumbnail.jpg",
+    name: "Product 4",
+  },
+];
+
+export default img;
